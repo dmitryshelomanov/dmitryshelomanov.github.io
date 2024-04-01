9ab2f3120eec52ef9f89970707d1decee331ec3a
DSHelomanov <dshelomanov@MacBook-Pro-Admin.local>
2024-04-01 19:21:59 +0300

    feat: added articles & more projects examples

---
+++ src/artcicles.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const articles = [
+  {
+    title: "Как использовать React Native навигатор",
+    link: "https://dmitryshelomanov.medium.com/how-to-use-react-navigation-814afa64dda3",
+    text: <>Базовое описание возможностей навигатора с примерами</>,
+  },
+  {
+    title: "Кастомный таббар в React Native навигатор",
+    link: "https://dmitryshelomanov.medium.com/react-navigation-custom-tabbar-78378c243a29",
+    text: <>Подробнее про навигацию в РН и создание кастомных анимаций</>,
+  },
+  {
+    title: "Snake AI",
+    link: "https://dmitryshelomanov.medium.com/snake-ai-7cb6d55c1b4e",
+    text: (
+      <>Описал подробнее примененные алгоритмы и логику работы визуализатора</>
+    ),
+  },
+];
